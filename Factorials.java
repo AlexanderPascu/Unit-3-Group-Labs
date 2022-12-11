b84c8173a81b10863773786cd70febd1aa87d89f
AlexanderPascu <111529601+AlexanderPascu@users.noreply.github.com>
2022-12-11 14:22:26 -0800

    Update Factorials.java

--- Factorials.java
+++ Factorials.java
@@ -1,3 +1,9 @@
+// Programmer: Alexander Pascu
+// Program Name: Factorials.java
+// Date: 12/08/2022
+// Version: 1.0
+// This program displays the factorial for every integer value from 1 to 10. 
+// A factorial of a number is the product of that number multiplied by each positive integer lower than it
 public class Factorials {
   public static void main(String[] args) {
     for (int i = 1; i <= 10; i++) {
